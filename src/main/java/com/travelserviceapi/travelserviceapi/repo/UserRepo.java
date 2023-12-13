4ecf29f14d5a7eb48c39026f8b64e28a2181b517
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-13 09:22:26 +0530

    add EntryNotFoundException class

--- src/main/java/com/travelserviceapi/travelserviceapi/repo/UserRepo.java
+++ src/main/java/com/travelserviceapi/travelserviceapi/repo/UserRepo.java
@@ -3,5 +3,8 @@ package com.travelserviceapi.travelserviceapi.repo;
 import com.travelserviceapi.travelserviceapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,String> {
+
 }
--- src/main/java/com/travelserviceapi/travelserviceapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/travelserviceapi/travelserviceapi/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.travelserviceapi.travelserviceapi.dto.requestDto.RequestUserDto;
 import com.travelserviceapi.travelserviceapi.dto.responseDto.ResponseUserDto;
 import com.travelserviceapi.travelserviceapi.repo.UserRepo;
 import com.travelserviceapi.travelserviceapi.service.UserService;
+import com.travelserviceapi.travelserviceapi.util.Generator;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,17 +16,27 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
 
+    private final Generator generator;
     private final ModelMapper mapper;
-    public UserServiceImpl(UserRepo userRepo, ModelMapper mapper) {
+    public UserServiceImpl(UserRepo userRepo, Generator generator, ModelMapper mapper) {
         this.userRepo = userRepo;
+        this.generator = generator;
         this.mapper = mapper;
     }
 
     @Override
     public ResponseUserDto SaveUser(RequestUserDto dto) {
-
         UserDto userDto = mapper.map(dto, UserDto.class);
+        String generatePrefix = generator.generatePrefix(5, 16);
+        String primaryKey = generator.generatePrimaryKey("USER-", generatePrefix);
+        userDto.setUserId(primaryKey);
+        if(!userRepo.existsById(userDto.getUserId())){
 
+        }else {
+
+        }
+
+        return  null;
 
     }
 }
