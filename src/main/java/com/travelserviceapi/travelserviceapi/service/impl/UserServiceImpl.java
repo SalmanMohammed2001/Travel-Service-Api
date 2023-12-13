88db65012bf75303e7e5e0acda9a50598f334ad4
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-13 08:50:35 +0530

    implement UserServiceImpl Method

---
+++ src/main/java/com/travelserviceapi/travelserviceapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.travelserviceapi.travelserviceapi.service.impl;
+
+import com.travelserviceapi.travelserviceapi.dto.requestDto.RequestUserDto;
+import com.travelserviceapi.travelserviceapi.dto.responseDto.ResponseUserDto;
+import com.travelserviceapi.travelserviceapi.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Override
+    public ResponseUserDto SaveUser(RequestUserDto dto) {
+        return null;
+    }
+}
