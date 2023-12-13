a008eb89488eecc42614487f0e1b26ae51646eb8
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-13 09:24:24 +0530

    add DuplicateEntryException class

--- src/main/java/com/travelserviceapi/travelserviceapi/adviser/AppWiderExceptionHandler.java
+++ src/main/java/com/travelserviceapi/travelserviceapi/adviser/AppWiderExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.travelserviceapi.travelserviceapi.adviser;
 
-import com.devstack.pos.exception.EntryNotFoundException;
-import com.devstack.pos.util.StandardResponse;
+
+import com.travelserviceapi.travelserviceapi.exception.DuplicateEntryException;
 import com.travelserviceapi.travelserviceapi.exception.EntryNotFoundException;
 import com.travelserviceapi.travelserviceapi.util.StandResponse;
 import org.springframework.http.HttpStatus;
@@ -14,6 +14,14 @@ public class AppWiderExceptionHandler {
 
     @ExceptionHandler(EntryNotFoundException.class)
     public ResponseEntity<StandResponse> handleEntryNotFoundException(EntryNotFoundException e){
+        return new ResponseEntity<>(
+                new StandResponse(404,e.getMessage(),e), HttpStatus.NOT_FOUND
+        );
+
+    }
+
+    @ExceptionHandler(DuplicateEntryException.class)
+    public ResponseEntity<StandResponse> handleDuplicateEntryException(EntryNotFoundException e){
         return new ResponseEntity<>(
                 new StandResponse(404,e.getMessage(),e), HttpStatus.NOT_FOUND
         );
