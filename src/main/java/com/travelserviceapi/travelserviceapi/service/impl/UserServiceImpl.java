c0e7a58f171e0f393fd3072bdc5b8257fdc24eae
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-13 09:42:08 +0530

    SaveUser method create

--- src/main/java/com/travelserviceapi/travelserviceapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/travelserviceapi/travelserviceapi/service/impl/UserServiceImpl.java
@@ -3,12 +3,16 @@ package com.travelserviceapi.travelserviceapi.service.impl;
 import com.travelserviceapi.travelserviceapi.dto.core.UserDto;
 import com.travelserviceapi.travelserviceapi.dto.requestDto.RequestUserDto;
 import com.travelserviceapi.travelserviceapi.dto.responseDto.ResponseUserDto;
+import com.travelserviceapi.travelserviceapi.entity.User;
+import com.travelserviceapi.travelserviceapi.exception.DuplicateEntryException;
 import com.travelserviceapi.travelserviceapi.repo.UserRepo;
 import com.travelserviceapi.travelserviceapi.service.UserService;
 import com.travelserviceapi.travelserviceapi.util.Generator;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Base64;
 
 @Service
 @Transactional
@@ -31,8 +35,13 @@ public class UserServiceImpl implements UserService {
         String primaryKey = generator.generatePrimaryKey("USER-", generatePrefix);
         userDto.setUserId(primaryKey);
         if(!userRepo.existsById(userDto.getUserId())){
-
+            User user = mapper.map(userDto, User.class);
+            user.setUserProfilePic(Base64.getEncoder().encodeToString(userDto.getProfilePic()).getBytes());
+            user.setUserNicFrontImg(Base64.getEncoder().encodeToString(userDto.getNicFrontImg()).getBytes());
+            user.setUserNicRearImg(Base64.getEncoder().encodeToString(userDto.getNicRearImg()).getBytes());
+            userRepo.save(user);
         }else {
+            throw new DuplicateEntryException("Duplicate Primary Key");
 
         }
 
